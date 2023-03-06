21ae6699e8609b561f5dbaebf20c332a8339060c
Zack <izowmart7@gmail.com>
2023-03-06 19:42:21 +0300

    Worked on actitivy  feature on share module

--- app/src/main/java/com/example/connect/Connect.kt
+++ app/src/main/java/com/example/connect/Connect.kt
@@ -8,6 +8,7 @@ import timber.log.Timber
 class Connect : Application() {
     override fun onCreate() {
         super.onCreate()
+
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
